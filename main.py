19563d6113c3686adbf30b62448cf6ee6b424099
Dario <brosds@gmail.com>
2015-04-29 19:31:17 +0200

    Upload

---
+++ main.py
@@ -0,0 +1,25 @@
+__author__ = 'DarioStabili'
+
+import glob
+import os
+import platform
+
+def main():
+    root = ""
+    if platform.system() == "Linux":
+        print("Linux")
+        root = "/"
+    elif platform.system() == "Windows":
+        print("Windows")
+        root = "C:/"
+
+    for root, dirs, files in os.walk(root, topdown=True):
+        out_file = open("log_list.txt","w")
+        path = os.path.join(root,'*.log')
+        for f in glob.glob(path):
+            print(f)
+            out_file.write("f")
+        out_file.close()
+
+if __name__ == "__main__":
+    main()
